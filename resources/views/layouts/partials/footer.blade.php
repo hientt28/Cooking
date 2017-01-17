5737849bca1a564d5f9268f926bcc82b4381e21c
Phạm Hà <Phm H>
2017-01-17 15:35:10 +0700

    Layout

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- footer -->
+<div class="footer-bottom wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
+    <div class="container">
+        <p>{{ trans('layout.footer') }} <a href="http://w3layouts.com/"> {{ trans('layout.w3layout') }}</a></p>
+    </div>
+</div>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,43 @@
+<div class="header">
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="logo">
+                    <a class="navbar-brand" href="#">{{ trans('layout.title') }}</a>
+                </div>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+                <nav class="cl-effect-13" id="cl-effect-13">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="active">{{ trans('layout.home') }}</a></li>
+                        <li><a href="#">{{ trans('layout.blog') }}</a></li>
+                        <li><a href="#">{{ trans('layout.createPost') }}</a></li>
+                        <li><a href="#">{{ trans('layout.mailUs') }}</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li role="presentation" class="dropdown">
+                            <img src="images/default.png" alt="" id="avatar_display">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Username <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">{{ trans('layout.profile') }}</a></li>
+                                <li><a href="#">{{ trans('layout.logout') }}</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+    </div>
+</div>
