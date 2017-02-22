50bf67f1a95938757c93da221d88a61ad6e6a09d
Trịnh Hiền <Trnh Hin>
2017-02-22 10:28:53 +0700

    Template and fix migration

--- resources/assets/js/angularjs/controllers/EditProfile.js
+++
@@ -1,8 +0,0 @@
-app.controller('EditProfile', function($scope, UserService, $routeParams, $location) {
-    $scope.title = "Edit Profile";
-
-    $scope.userCurr = UserService.get({id: $routeParams.id});
-    $scope.errors = null;
-
-    $scope.disable_submit = false;
-});
--- resources/assets/js/angularjs/services/UserService.js
+++
@@ -1,5 +0,0 @@
-app.factory('UserService', function ($http, $resource, API_URL) {
-    return $resource(API_URL + '/users/:id', null, {
-        'update': { 'method':'PUT' }
-    });
-});
